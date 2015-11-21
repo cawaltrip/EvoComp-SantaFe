72f8fa8159814eb8091642d972fcc04cca42deea
Chris Waltrip <walt2178@vandals.uidaho.edu>
2015-11-20 18:46:58 -0800

    Blank header and solution files.

---
+++ EvoComp-SantaFe/operator_types.h
@@ -0,0 +1,26 @@
+/*
+* operator_types.h
+* UIdaho CS-572: Evolutionary Computation
+* Enumeration of potential operator types needed for Genetic Program
+*
+* Copyright (C) 2015 Chris Waltrip <walt2178@vandals.uidaho.edu>
+*
+* This file is part of EvoComp-SantaFe
+*
+* EvoComp-SantaFe is free software: you can redistribute it and/or modify
+* it under the terms of the GNU Affero General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+*
+* EvoComp-SantaFe is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU Affero General Public License for more details.
+*
+* You should have received a copy of the GNU Affero General Public License
+* along with EvoComp-SantaFe.  If not, see <http://www.gnu.org/licenses/>.
+*/
+#pragma once
+enum OpType {
+
+};
